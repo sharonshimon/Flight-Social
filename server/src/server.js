bb0564cde2c4fb590fba90560793af01c263a53a
stavsiv <sivilia2@gmail.com>
2025-09-12 20:39:32 +0300

    update connect to mongo db

--- server/src/server.js
+++ server/src/server.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected ✅');
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   })
   .catch(err => console.error('DB connection error', err));
 
-  // const User = mongoose.model('User', userSchema);
-  // module.exports = User;
+// const User = mongoose.model('User', userSchema);
+// module.exports = User;
